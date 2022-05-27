eef23ccef22b3754d6b626e75929f66f99a68d75
Emgan <emgan29@gmail.com>
2022-05-27 09:26:29 +0200

    done the implementation of the filtering and graphplot part into the core.py It can now filter the files with the given parameters from run.py and show the figures if we want to and save them into the res subfolder if we want to

--- src/graphplot.py
+++ src/graphplot.py
@@ -1,14 +1,16 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 class Plot:
 
-    def __init__(self,IV_X,IV_Y,SPC_X,SPC_Y,SPC_ref_fit):
+    def __init__(self,IV_X,IV_Y,SPC_X,SPC_Y,SPC_ref_fit, opt_showfig, opt_savefig):
         self.IV_X=IV_X
         self.IV_Y=IV_Y
         self.SPC_X=SPC_X
         self.SPC_Y=SPC_Y
         self.SPC_ref_fit=SPC_ref_fit
+        self.opt_showfig = opt_showfig
+        self.opt_savefig = opt_savefig
 
     def IV_ref_plot(self):
         plt.plot(self.IV_X, abs(self.IV_Y), 'bo--', label='Raw data')
@@ -62,7 +64,7 @@ class Plot:
         plt.plot(x_line,self.SPC_ref_fit[3](x_line),'y-',label='4th order')
         plt.plot(x_line,self.SPC_ref_fit[4](x_line),'c-',label='5th order')
         plt.plot(x_line,self.SPC_ref_fit[5](x_line),'b--',label='6th order')
-        plt.plot(self.SPC_X,self.SPC_Y,'k--',label='Raw data')
+        plt.plot(self.SPC_X[6],self.SPC_Y[6],'k--',label='Raw data')
         plt.xlabel('wavelenght [nm]')
         plt.ylabel('Measured transmission[dB]')
         plt.title('Transmission spectra - As mesured')
@@ -73,8 +75,34 @@ class Plot:
         for i in range(0, 7):
             z = np.polyfit(self.SPC_X[6],self.SPC_Y[6],3)
             p = np.poly1d(z)
-        plt.plot(self.SPC_X[i][:6065],self.SPC_Y[i][:6065] - p(self.SPC_X[6]))
+            plt.plot(self.SPC_X[i][:6065],self.SPC_Y[i][:6065] - p(self.SPC_X[6]))
         plt.xlabel('wavelenght [nm]')
         plt.ylabel('Measured transmission[dB]')
         plt.title('flatten Transmission spectra - as measured')
-        plt.axis([1527, 1583, -65, 0])
+        plt.axis([1527, 1583, -65, 5])
+
+    # Data analysis
+    def data_analysis_plot(self,x_1, x_2, x_3, fittedX_1, fittedX_2, lm_coef, filename):        
+        # 1. IV plot
+        plt.figure(figsize=(20,8))
+        plt.subplot(2,2,1)
+        self.IV_ref_plot()
+        self.IV_ployfit_plot(x_1,fittedX_1)
+        self.IV_ployfit_plot(x_2,fittedX_2)
+        self.IV_lmfit_plot(x_3,lm_coef)
+        # 2. Wavelength(reference) plot
+        plt.subplot(2,2,2)
+        self.raw_data()
+        # 3. reference fitting
+        plt.subplot(2,2,3)
+        self.ref_fit_plot()
+        # 4. flatten spectrum
+        plt.subplot(2,2,4)
+        self.flatten_data()
+        plt.suptitle(f'{filename}')
+        # we look if we have to show the figure and if we have to save it
+        if self.opt_savefig == True:
+            print()
+            plt.savefig(f'./../res/jpgs/{filename}')
+        elif self.opt_showfig == True:
+            plt.show()
